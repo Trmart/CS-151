6e6de74001af9c93cc822e54876f7cf886c5098b
Taylor Martin <57766633+Trmart@users.noreply.github.com>
2022-03-14 15:12:39 -0700

    Delete Quick Sort directory

--- Quick Sort/quicksort.h
+++
@@ -1,9 +0,0 @@
-#ifndef QUICK_H
-#define QUICK_H
-#include <iostream>
-//#include <algorithm> // needed for the swap function 
-
-void sortShit(int [], int, int); //recursive quick sort function 
-int cut_A_Bitch(int[], int, int);  //partition function 
-void swapShit(int &i, int &j); 
-#endif
